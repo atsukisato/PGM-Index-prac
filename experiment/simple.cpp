5093932986f19a1199f21bdaf446410f9bceb605
chikkun0310 <sato.a.20010310@gmail.com>
2022-05-22 17:06:25 +0900

    measure size

--- experiment/simple.cpp
+++ experiment/simple.cpp
@@ -12,20 +12,28 @@
 #include <algorithm>
 #include <time.h>
 #include <cmath>
+
 #include "pgm/pgm_index.hpp"
 
 
 const int epsilon = 128;
 
-std::vector<double> measure_build_times(const std::vector<int> &data, const int build_num = 10){
+template<typename T>
+std::vector<double> measure_build_times(const std::vector<T> &data, const int build_num = 10){
     std::vector<double> build_times;
     for(int i = 0; i < build_num; i++){
         clock_t start = clock();
-        pgm::PGMIndex<int, epsilon> index(data);
+        pgm::PGMIndex<T, epsilon> index(data);
         clock_t end = clock();
         build_times.push_back(static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0);
     }
     return build_times;
+}
+
+template<typename T>
+double measure_size(const std::vector<T> &data){
+    pgm::PGMIndex<T, epsilon> index(data);
+    return (double)index.size_in_bytes();
 }
 
 void show_ave_std(std::vector<double> vec){
@@ -42,21 +50,37 @@ void show_ave_std(std::vector<double> vec){
     std::cout << "std:" << std << std::endl;
 }
 
+
+std::vector<double> make_random_double_data(const int data_length = 1000000, const double fMin = 0.0, const double fMax = 1.0){
+    std::vector<double> data;
+    for(int i = 0; i < data_length; i++){
+        double f = (double)rand() / RAND_MAX;
+        double rn = fMin + f * (fMax - fMin);
+        data.push_back(rn);
+    }
+    sort(data.begin(), data.end());
+    return data;
+}
+
 int main() {
-    const int data_length = 1000000;
+    const int data_length = 100000;
     const int build_num = 10;
 
     // the case of random data
-    std::vector<int> random_data(data_length);
-    std::generate(random_data.begin(), random_data.end(), std::rand);
-    std::sort(random_data.begin(), random_data.end());    
+    std::vector<double> random_data = make_random_double_data();
 
     std::vector<double> build_times = measure_build_times(random_data, build_num);
     std::cout << "--- random data ---" << std::endl;
     show_ave_std(build_times);
 
-    // the case of fgsm
-
+    std::vector<double> hoge;
+    for(int i=0;i<10;i++){
+        double siz = measure_size(random_data);
+        hoge.push_back(siz);
+    }
+    std::cout << "--- random data ---" << std::endl;
+    show_ave_std(hoge);
+    
 
     return 0;
 }
