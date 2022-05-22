f5a5fcd05b33437f9c26469225f99425a2badcdc
chikkun0310 <sato.a.20010310@gmail.com>
2022-05-22 15:28:59 +0900

    measure_build_times

--- experiment/simple.cpp
+++ experiment/simple.cpp
@@ -10,25 +10,46 @@
 #include <cstdlib>
 #include <iostream>
 #include <algorithm>
+#include <time.h>
+#include <cmath>
 #include "pgm/pgm_index.hpp"
+
+
+const int epsilon = 128;
+
+std::vector<double> measure_build_times(std::vector<int> data, const int build_num = 10){
+    std::vector<double> build_times;
+    for(int i = 0; i < build_num; i++){
+        clock_t start = clock();
+        pgm::PGMIndex<int, epsilon> index(data);
+        clock_t end = clock();
+        build_times.push_back(static_cast<double>(end - start) / CLOCKS_PER_SEC * 1000.0);
+    }
+    return build_times;
+}
+
+void show_ave_std(std::vector<double> vec){
+    int n = (int)vec.size();
+    double sum = 0.0;
+    double sum2 = 0.0;
+    for(int i = 0; i < n; i++){
+        sum += vec[i];
+        sum2 += vec[i] * vec[i];
+    }
+    double ave = sum / n;
+    double std = std::sqrt(sum2 / n - ave * ave);
+    std::cout << "ave:" << ave << std::endl;
+    std::cout << "std:" << std << std::endl;
+}
 
 int main() {
     // Generate some random data
     std::vector<int> data(1000000);
     std::generate(data.begin(), data.end(), std::rand);
-    data.push_back(42);
     std::sort(data.begin(), data.end());
 
-    // Construct the PGM-index
-    const int epsilon = 128; // space-time trade-off parameter
-    pgm::PGMIndex<int, epsilon> index(data);
-
-    // Query the PGM-index
-    auto q = 42;
-    auto range = index.search(q);
-    auto lo = data.begin() + range.lo;
-    auto hi = data.begin() + range.hi;
-    std::cout << *std::lower_bound(lo, hi, q);
+    std::vector<double> build_times = measure_build_times(data, 10);
+    show_ave_std(build_times);
 
     return 0;
 }
